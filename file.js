a2ca3bd45e695a68f7ba6ae65a55d6092b175a3e
Felp7101 <felipeas77101@gmail.com>
2024-03-30 00:11:47 -0300

    consetado o erro do header

---
+++ file.js
@@ -0,0 +1,13 @@
+const maria =  { 
+    name:"Maria Clara",
+    age:22,
+    email:"mariaclara000@hotlook",
+    address: {street:"Antonia cruzes",
+    country:"Brazil",
+    state: "São Paulo",
+    city: "SP",
+}
+}
+maria.age = 23
+
+console.log (maria)
